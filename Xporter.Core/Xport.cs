13ffecb05b0cfd4bbab739a0cdbd478b9d4e2c7a
Dimitris Grevenos <dim.grev@outlook.com>
2021-03-10 15:29:58 +0200

    rmv unused "usings"

--- Xporter.Core/Xport.cs
+++ Xporter.Core/Xport.cs
@@ -1,10 +1,7 @@
 ﻿using OfficeOpenXml;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace Xporter
 {
--- XporterConsole/Program.cs
+++ XporterConsole/Program.cs
@@ -1,5 +1,4 @@
-﻿using OfficeOpenXml;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using Xporter;
